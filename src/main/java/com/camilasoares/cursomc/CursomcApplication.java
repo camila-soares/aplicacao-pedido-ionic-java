25d72aee9a4574d085041da58ac4e7c99dfd582d
Camila Soares <camilasoares1507@gmail.com>
2018-04-29 22:46:12 -0300

    criando opração de instanciação

--- src/main/java/com/camilasoares/cursomc/CursomcApplication.java
+++ src/main/java/com/camilasoares/cursomc/CursomcApplication.java
@@ -1,12 +1,35 @@
 package com.camilasoares.cursomc;
 
+import java.lang.reflect.Array;
+
+import org.assertj.core.util.Arrays;
+import org.hibernate.validator.constraints.SafeHtml.Attribute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.camilasoares.cursomc.domain.Category;
+import com.camilasoares.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 }
