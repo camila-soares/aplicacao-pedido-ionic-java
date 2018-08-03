53d1f3c4e6a49cadb5c618a51ff9b938ea01ccdb
Camil Soares <camilasoares1507@gmail.com>
2018-08-03 16:25:21 -0300

    ajustes

--- src/main/java/com/camilasoares/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/camilasoares/cursomc/services/SmtpEmailService.java
@@ -4,6 +4,8 @@ import com.camilasoares.cursomc.domain.Client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -12,6 +14,20 @@ public class SmtpEmailService extends AbstractEmailService {
     @Autowired
     private MailSender mailSender;
 
+    @Bean
+    public MailSender mailSender() {
+        return new MailSender () {
+            @Override
+            public void send(SimpleMailMessage simpleMailMessage) throws MailException {
+                
+            }
+
+            @Override
+            public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
+
+            }
+        };
+    }
 
 
     private static final Logger LOG = LoggerFactory.getLogger ( SmtpEmailService.class );
