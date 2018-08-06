f57144aac2339fb8659334478159a1746c778ac3
Camil Soares <camilasoares1507@gmail.com>
2018-08-06 11:43:23 -0300

    corrigindo conflitos

--- src/main/java/com/camilasoares/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/camilasoares/cursomc/services/SmtpEmailService.java
@@ -1,11 +1,8 @@
 package com.camilasoares.cursomc.services;
 
-import com.camilasoares.cursomc.domain.Client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
@@ -13,21 +10,6 @@ public class SmtpEmailService extends AbstractEmailService {
 
     @Autowired
     private MailSender mailSender;
-
-    @Bean
-    public MailSender mailSender() {
-        return new MailSender () {
-            @Override
-            public void send(SimpleMailMessage simpleMailMessage) throws MailException {
-                
-            }
-
-            @Override
-            public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
-
-            }
-        };
-    }
 
     private static final Logger LOG = LoggerFactory.getLogger ( SmtpEmailService.class );
 
@@ -38,8 +20,5 @@ public class SmtpEmailService extends AbstractEmailService {
         LOG.info ( "Email enviado" );
     }
 
-    @Override
-    public void sendNewPasswordEmail(Client client , String newPass) {
-
-    }
+    
 }
